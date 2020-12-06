cf36beede4212e9f06d98518a4faa1e48636e569
Andrewkar7 <andrey141294@yandex.ru>
2020-12-06 15:54:48 +0300

    дз сразу к 6 и 7 урокам

--- simple_wsgi.py
+++ simple_wsgi.py
@@ -1,13 +1,20 @@
 # Для запуска используем gunicorn
 # gunicorn simple_wsgi:application
 
-from framy.heart import Application, DebugApplication, FakeApplication
+from framy.heart import Application
 from framy.templator import render
-from models import OurSite
+from framy.framycbv import CreateView, ListView
+from models import OurSite, BaseSerializer, EmailNotifier, SmsNotifier
 from logging_our import Logger, debug
+from framyorm import UnitOfWork
+from mappers import MapperRegistry
 
 site = OurSite()
 logger = Logger('wsgi')
+email_notifier = EmailNotifier()
+sms_notifier = SmsNotifier()
+UnitOfWork.new_current()
+UnitOfWork.get_current().set_mapper_registry(MapperRegistry)
 
 
 def main_view(request):
@@ -20,40 +27,92 @@ def create_course(request):
     if request['method'] == 'POST':
         data = request['data']
         name = data['name']
-        category_id = data.get['category_id']
+        category_id = data.get('category_id')
         print(category_id)
         if category_id:
             category = site.find_category_by_id(int(category_id))
 
             course = site.create_course('record', name, category)
+            course.observers.append(email_notifier)
+            course.observers.append(sms_notifier)
             site.courses.append(course)
-        return '200 OK', render('create_course.html')
+        categories = site.categories
+        return '200 OK', render('create_course.html', categories=categories)
     else:
         categories = site.categories
         return '200 OK', render('create_course.html', categories=categories)
 
 
-def create_category(request):
-    if request['method'] == 'POST':
-        data = request['data']
+class CategoryCreateView(CreateView):
+    template_name = 'create_category.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['categories'] = site.categories
+        return context
+
+    def create_obj(self, data: dict):
         name = data['name']
-        category_id = data.get['category_id']
+        category_id = data.get('category_id')
+
         category = None
         if category_id:
             category = site.find_category_by_id(int(category_id))
 
-        new_category = site.create_course(name, category)
+        new_category = site.create_category(name, category)
         site.categories.append(new_category)
-        return '200 OK', render('create_category.html')
-    else:
-        categories = site.categories
-        return '200 OK', render('create_category.html', categories=categories)
+
+
+class CategoryListView(ListView):
+    queryset = site.categories
+    template_name = 'category_list.html'
+
+
+class StudentListView(ListView):
+    queryset = site.students
+    template_name = 'student_list.html'
+
+    def get_queryset(self):
+        mapper = MapperRegistry.get_current_mapper('student')
+        return mapper.all()
+
+
+class StudentCreateView(CreateView):
+    template_name = 'create_student.html'
+
+    def create_obj(self, data: dict):
+        name = data['name']
+        new_obj = site.create_user('student', name)
+        site.students.append(new_obj)
+        new_obj.mark_new()
+        UnitOfWork.get_current().commit()
+
+
+class AddStudentByCourseCreateView(CreateView):
+    template_name = 'add_student.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['courses'] = site.courses
+        context['students'] = site.students
+        return context
+
+    def create_obj(self, data: dict):
+        course_name = data['course_name']
+        course = site.get_course(course_name)
+        student_name = data['student_name']
+        student = site.get_student(student_name)
+        course.add_student(student)
 
 
 routes = {
     '/': main_view,
     '/create-course/': create_course,
-    '/create-category/': create_category,
+    '/create-category/': CategoryCreateView(),
+    '/category-list/': CategoryListView(),
+    '/student-list/': StudentListView(),
+    '/create-student/': StudentCreateView(),
+    '/add-student/': AddStudentByCourseCreateView(),
 }
 
 
@@ -64,6 +123,8 @@ def secret_front(request):
 fronts = [secret_front]
 
 application = Application(routes, fronts)
+
+
 # application = DebugApplication(routes, fronts)
 # application = FakeApplication(routes, fronts)
 
@@ -81,7 +142,6 @@ def copy_course(request):
     return '200 OK', render('course_list.html', objects_list=site.courses)
 
 
-@application.add_route('/category-list/')
-def category_list(request):
-    logger.log('Список категорий')
-    return '200 OK', render('category_list.html', objects_list=site.categories)
+@application.add_route('/api/')
+def course_api(request):
+    return '200 OK', BaseSerializer(site.courses).save()
